948817bf243d0248b1aab0098adade6b88899813
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2021-11-14 22:10:44 +0100

    Configure storybook preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import { ThemeProvider } from "styled-components";
+import { theme, darkTheme } from "assets/styles/themes";
+import GlobalStyle from "assets/styles/globalStyle";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +10,35 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+export const globalTypes = {
+  theme: {
+    name: "Theme",
+    description: "Global theme for components",
+    defaultValue: "light",
+    toolbar: {
+      icon: "circlehollow",
+      items: ["light", "dark"],
+      showName: true,
+    },
+  },
+};
+
+const getTheme = (themeName) => {
+  const themes = {
+    light: theme,
+    dark: darkTheme,
+  };
+  return themes[themeName];
+};
+
+const withThemeProvider = (Story, context) => {
+  const theme = getTheme(context.globals.theme);
+  return (
+    <ThemeProvider theme={theme}>
+      <Story {...context} />
+    </ThemeProvider>
+  );
+};
+export const decorators = [withThemeProvider];
