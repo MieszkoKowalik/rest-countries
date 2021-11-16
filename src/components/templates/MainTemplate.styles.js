6123b7d0efd8ded751f43e45d17de20ae8d83786
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2021-11-16 13:24:56 +0100

    add prefered color scheme detection to themes

--- src/components/templates/MainTemplate.styles.js
+++ src/components/templates/MainTemplate.styles.js
@@ -4,4 +4,5 @@ export const Wrapper = styled.div`
   min-height: 100vh;
   display: grid;
   grid-template-rows: auto 1fr;
+  background-color: ${({ theme }) => theme.colors.neutral};
 `;
--- src/providers/ThemesProvider.js
+++ src/providers/ThemesProvider.js
@@ -1,12 +1,19 @@
 import { ThemeProvider } from "styled-components";
 import { theme, darkTheme } from "assets/styles/themes";
 import { useState } from "react";
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 
 export const ThemesContext = createContext();
 
 const ThemesProvider = ({ children }) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+    if (prefersDarkScheme.matches) {
+      setIsDarkMode(true);
+    }
+  }, []);
 
   return (
     <ThemesContext.Provider value={{ isDarkMode, setIsDarkMode }}>
