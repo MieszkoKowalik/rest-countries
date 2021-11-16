7b4a965bbbb24d7c091aab2f4c1227b136acc89f
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2021-11-16 19:44:04 +0100

    Add routing and main views

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,15 +1,24 @@
 import GlobalStyle from "assets/styles/globalStyle";
 import ThemesProvider from "providers/ThemesProvider";
 import MainTemplate from "components/templates/MainTemplate";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router";
+import Countries from "views/Countries/Countries";
+import Country from "views/Country/Country";
 
 function Root() {
   return (
-    <>
+    <BrowserRouter>
       <ThemesProvider>
         <GlobalStyle />
-        <MainTemplate></MainTemplate>
+        <MainTemplate>
+          <Routes>
+            <Route path="/" element={<Countries />} />
+            <Route path="/country/:name" element={<Country />} />
+          </Routes>
+        </MainTemplate>
       </ThemesProvider>
-    </>
+    </BrowserRouter>
   );
 }
 
