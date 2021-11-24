bd43c454e85f58f3d859882deadce038b8728a10
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2021-11-24 22:11:49 +0100

    Add color property to CountryDetail

--- src/components/atoms/CountryDetail/CountryDetail.styles.js
+++ src/components/atoms/CountryDetail/CountryDetail.styles.js
@@ -3,10 +3,10 @@ import styled from "styled-components";
 export const StyledP = styled.p`
   font-size: 0.875rem;
   font-weight: 300;
+  color: ${({ theme }) => theme.colors.primary};
   span {
     font-weight: 600;
   }
-
   @media ${({ theme }) => theme.media.medium} {
     ${({ isResponsive }) => isResponsive && `font-size:1rem`}
   }
