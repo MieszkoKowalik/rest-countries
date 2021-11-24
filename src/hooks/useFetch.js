52e6b29b5339e8419c840ebc4dfec004d6667a05
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2021-11-24 16:41:12 +0100

    Add useFetch custom hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+const useFetch = (initialUrl, initialParams = {}, skip = false) => {
+  const [url, updateUrl] = useState(initialUrl);
+  const [params, updateParams] = useState(initialParams);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const queryString = Object.keys(params)
+    .map(
+      (key) =>
+        "?" + encodeURIComponent(key) + "=" + encodeURIComponent(params[key])
+    )
+    .join("&");
+  useEffect(() => {
+    const abortController = new AbortController();
+    const fetchData = async () => {
+      if (skip) return;
+
+      try {
+        const response = await fetch(`${url}${queryString}`, {
+          signal: abortController.signal,
+        });
+        const result = await response.json();
+        if (response.ok) {
+          setData(result);
+        } else {
+          setHasError(true);
+          setErrorMessage(result);
+        }
+        setIsLoading(false);
+      } catch (err) {
+        setHasError(true);
+        setErrorMessage(err.message);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+
+    return () => {
+      abortController.abort();
+    };
+  }, [url, params, queryString, skip]);
+  return { data, isLoading, hasError, errorMessage, updateUrl, updateParams };
+};
+export default useFetch;
